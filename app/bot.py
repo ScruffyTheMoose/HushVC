2ee081d855adb7f084a496e6a6c6a738882190a9
ScruffyTheMoose <91182195+ScruffyTheMoose@users.noreply.github.com>
2023-05-05 12:10:53 -0500

    update bot.py to working build

--- app/bot.py
+++ app/bot.py
@@ -1,64 +1,58 @@
 import discord
+from discord.ext import commands
+from custom_core import StreamSink
 
-# opening all intents except for privilaged
+
 intents = discord.Intents.all()
-intents.members = False
-intents.presences = False
+bot = commands.Bot(command_prefix='!', intents=intents)
 
-# instantiating discord client
-client = discord.Client(intents=intents)
+connections = {}
 
-
-async def handle_transcription(transcription):
-    print(transcription)
+stream_sink = StreamSink()
 
 
-async def transcribe_audio(audio_data):
-    # initialize WhisperAI
-    # ai = whisper.load_model("tiny")
+@bot.command()
+async def record(ctx):  # if you're using commands.Bot, this will also work.
+    voice = ctx.author.voice
 
-    # stream the audio data and transcribe in real-time
-    async with ai.streaming_transcribe(audio_data) as result_stream:
-        async for result in result_stream:
-            # handle the transcription in real-time
-            await handle_transcription(result.transcription)
+    if not voice:
+        # hehe
+        await ctx.reply("You aren't in a voice channel, get your life together lmao")
 
+    # connect to the voice channel the author is in.
+    stream_sink.set_user(ctx.author.id)
+    vc = await voice.channel.connect()
+    # updating the cache with the guild and channel.
+    connections.update({ctx.guild.id: vc})
 
-@client.event
-async def on_ready():
-    print(f"Logged in as {client.user}")
+    vc.start_recording(
+        stream_sink,  # the sink type to use.
+        once_done,  # what to do once done.
+        ctx.channel  # the channel to disconnect from.
+    )
 
-
-@client.event
-async def on_message(message):
-    if message.content.startswith("~!join"):
-        # need error catching here incase user not in channel
-        channel = message.author.voice.channel
-
-        vc = await channel.connect()
-        while vc.is_connected():
-            audiostrean = (
-                discord.AudioSource()
-            )  # read audio data from the voice channel
-            audio_data = discord.PCMAudio(audiostrean)
-
-            chunk = audio_data.read()
-
-            print(type(chunk))
-
-            await transcribe_audio(
-                audio_data
-            )  # send audio data to WhisperAI for live transcription
-
-        await vc.disconnect()
-
-    elif message.content.startswith("~!leave"):
-        if message.guild.voice_client:
-            await message.guild.voice_client.disconnect()
+    await ctx.reply("Started listening.")
 
 
-# reading bot token from outside file
+# our voice client already passes these in.
+async def once_done(sink: discord.sinks, channel: discord.TextChannel, *args):
+    await sink.vc.disconnect()  # disconnect from the voice channel.
+    print("Stopped listening.")
+
+
+@bot.command()
+async def stop_recording(ctx):
+    if ctx.guild.id in connections:  # check if the guild is in the cache.
+        vc = connections[ctx.guild.id]
+        # stop recording, and call the callback (once_done).
+        vc.stop_recording()
+        del connections[ctx.guild.id]  # remove the guild from the cache.
+    else:
+        # respond with this if we aren't listening
+        await ctx.reply("I am currently not listening here.")
+
+# reading in token
 key = open("key.pw", "r").read()
 
 if __name__ == "__main__":
-    client.run(key)
+    bot.run(key)
