64d76284f39921b1cf5f555317e1fd19bb9870fc
ScruffyTheMoose <91182195+ScruffyTheMoose@users.noreply.github.com>
2023-05-06 13:08:06 -0500

    fix minor reference error, rm example code

--- transcription/model.py
+++ transcription/model.py
@@ -5,23 +5,6 @@ import numpy as np
 # Reference:
 # https://huggingface.co/docs/transformers/main/en/model_doc/whisper#overview
 
-# load model and processor
-processor = WhisperProcessor.from_pretrained("openai/whisper-tiny")
-model = WhisperForConditionalGeneration.from_pretrained("openai/whisper-tiny")
-model.config.forced_decoder_ids = None
-
-
-input_features = processor(
-    sample["array"], sampling_rate=sample["sampling_rate"], return_tensors="pt").input_features
-
-# generate token ids
-predicted_ids = model.generate(input_features)
-# decode token ids to text
-transcription = processor.batch_decode(
-    predicted_ids, skip_special_tokens=False)
-
-transcription = processor.batch_decode(predicted_ids, skip_special_tokens=True)
-
 
 class Whisper:
     def __init__(self, **kwargs) -> None:
@@ -41,7 +24,7 @@ class Whisper:
 
         pred_ids = self.model.generate(input_features)
 
-        transcription = processor.batch_decode(
+        transcription = self.processor.batch_decode(
             pred_ids, skip_special_tokens=False)
 
         return transcription
